00b3dbd919efade0c4a10c7b27c5462c5249349f
ky5697 -007 <ky5697@srmist.edu.in>
2026-03-17 08:48:22 +0530

    Koushik added UC4 to git

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,11 @@
-public class HelloApp {
-    public static void main (String[] args){
-        String name = "World";
-        if (args.length > 0){
-            name = args[0];
+public class HelloApp{
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
         }
-        
-        System.out.println("Hello " + name +"!");
     }
 }
