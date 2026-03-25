e9fb22e8f268c80656ad3e38386b06af662768da
ky5697 -007 <ky5697@srmist.edu.in>
2026-03-25 20:55:57 +0530

    Koushik added to git

--- HelloApp1/src/main/java/org/example/HelloApp.java
+++ HelloApp1/src/main/java/org/example/HelloApp.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class HelloApp {
-    public static void main (String[] args){
-
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+            String name = args[0];  // Get the first argument
+            System.out.println("Hello, " + name + "!");
     }
 }
