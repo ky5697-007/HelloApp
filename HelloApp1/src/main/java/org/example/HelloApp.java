4b479f492819cb859628ac0c8aceff96154f9c6c
ky5697 -007 <ky5697@srmist.edu.in>
2026-03-25 21:04:45 +0530

    UC3 Implementation

--- HelloApp1/src/main/java/org/example/HelloApp.java
+++ HelloApp1/src/main/java/org/example/HelloApp.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-            String name = args[0];  // Get the first argument
-            System.out.println("Hello, " + name + "!");
+
+        String name = world;
+
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
